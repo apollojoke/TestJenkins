be7392db37d1d204b983d27da96909b8ac742b73
Jun Lee <li.jun4454@gmail.com>
2015-03-09 17:37:07 +0800

    add say hi method

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,4 +2,7 @@ public class Hello {
     public String sayHello(String name){
         return "Hello, " + name;
     }
+    public String sayHi(String name){
+        return "Hi, " + name;
+    }
 }
--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,11 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class HelloTest {
+    private Hello hello;
+    @Before
+    public void setUp() throws Exception {
+        hello = new Hello();
+    }
 
-    @org.junit.Test
+    @Test
     public void testSayHello() throws Exception {
-        Hello hello = new Hello();
         assertThat(hello.sayHello("Jun"), is("Hello, Jun"));
+    }
+
+    @Test
+    public void testSayHi() throws Exception {
+        assertThat(hello.sayHi("Jun"), is("Hi, Jun"));
+
     }
 }
